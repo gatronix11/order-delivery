adf5e1ccda796dc7ec692dc43b2c621ec19705a9
gatronix11 <gatronix11@gmail.com>
2024-08-14 22:31:34 +0530

    connecting to mongo atlas

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ const routes = require('./routes');
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/deliveryOrders', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+const dbURI = process.env.MONGODB_URI || 'mongodb+srv://gt11:gtx123@cluster0.km5cz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB Atlas'))
+    .catch(err => console.log(err));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
@@ -20,6 +20,4 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
