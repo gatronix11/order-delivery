593ec6527b40edbfd1b9f0189a11f2808ad58737
gatronix11 <gatronix11@gmail.com>
2024-08-15 07:43:55 +0530

    Delete models/orderNumber.js

--- models/orderNumber.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const orderNumberSchema = new mongoose.Schema({
-    lastOrderNumber: { type: Number, default: 0 }
-});
-
-module.exports = mongoose.model('OrderNumber', orderNumberSchema);
