61db39d0059d068dae790e742ba9b4ed95b510aa
gatronix11 <gatronix11@gmail.com>
2024-08-15 07:43:43 +0530

    Update routes.js

--- routes.js
+++ routes.js
@@ -4,7 +4,6 @@ const Order = require('./models/order');
 const pdf = require('html-pdf');
 const ejs = require('ejs');
 const path = require('path');
-const OrderNumber = require('./models/OrderNumber');
 
 
 // Route to render the add order page
@@ -16,25 +15,10 @@ router.get('/add-order', (req, res) => {
 router.post('/add-order', async (req, res) => {
     const { orderNumber, customerName, address, contactNumber, product, price, shippingMethod } = req.body;
     
-    let newOrderNumber;
-    if (orderNumber && !isNaN(orderNumber)) {
-        newOrderNumber = parseInt(orderNumber, 10);
-    } else {
-            // Find the last order number and increment it
-        let orderNumberDoc = await OrderNumber.findOne();
-        if (!orderNumberDoc) {
-                // If no document exists, create one
-            orderNumberDoc = new OrderNumber({ lastOrderNumber: 0 });
-        }
-        newOrderNumber = orderNumberDoc.lastOrderNumber + 1;
-        orderNumberDoc.lastOrderNumber = newOrderNumber;
-        await orderNumberDoc.save();
-    }
-    
     const orderDate = new Date();
     orderDate.setMinutes(orderDate.getMinutes() + 330);
     
-    const newOrder = new Order({ orderNumber: newOrderNumber, customerName, address, contactNumber, product, price, shippingMethod, orderDate });
+    const newOrder = new Order({ orderNumber, customerName, address, contactNumber, product, price, shippingMethod, orderDate });
     
     await newOrder.save();
     res.redirect('/view-orders');
